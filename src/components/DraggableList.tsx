edd31bc4ca79ad037f10c8ef78d5a399e4d1abb6
KAMRUZZAMAN <kzamanbn@gmail.com>
2023-11-29 00:51:54 +0600

    Update DraggableList.tsx

--- src/components/DraggableList.tsx
+++ src/components/DraggableList.tsx
@@ -108,7 +108,7 @@ const DraggableList: FC = () => {
     };
 
     return (
-        <div className="card border">
+        <div className="card border my-5">
             <h2 className="card-header flex justify-between">
                 <span className="text-2xl font-bold ">{cardTitle}</span>
                 {selectedItems > 0 && (
@@ -137,72 +137,45 @@ const DraggableList: FC = () => {
                                     checked={Boolean(checked)}
                                 />
                             ))}
-                            <div className="card-item border-2 border-gray-300 border-dashed p-4 bg-gray-50 hover:bg-gray-100 lg:col-span-1 md:col-span-1 col-span-2">
-                                <div className="flex items-center justify-center w-full">
-                                    <Dropzone onDrop={handleDrop} multiple>
-                                        {({ getRootProps, getInputProps }) => (
-                                            <div {...getRootProps()}>
-                                                <input {...getInputProps()} />
-                                                <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                                                    <svg
-                                                        className="w-8 h-8 mb-4 text-gray-500 "
-                                                        aria-hidden="true"
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        fill="none"
-                                                        viewBox="0 0 20 16">
-                                                        <path
-                                                            stroke="currentColor"
-                                                            strokeLinecap="round"
-                                                            strokeLinejoin="round"
-                                                            strokeWidth="2"
-                                                            d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                                                        />
-                                                    </svg>
-                                                    <p className="mb-2 text-sm text-gray-500 ">
-                                                        <span className="font-semibold">Add Image</span>
-                                                    </p>
-                                                    <p className="text-xs text-gray-500 flex justify-center">
-                                                        PNG, JPG or GIF (1:1)
-                                                    </p>
-                                                </div>
+                            <div className="card-item border-2 border-gray-300 border-dashed bg-gray-50 hover:bg-gray-100 lg:col-span-1 md:col-span-1 col-span-2">
+                                <Dropzone
+                                    onDrop={handleDrop}
+                                    multiple
+                                    accept={{
+                                        'image/jpeg': [],
+                                        'image/png': [],
+                                        'image/gif': []
+                                    }}>
+                                    {({ getRootProps, getInputProps }) => (
+                                        <div
+                                            {...getRootProps()}
+                                            className="flex items-center justify-center h-full w-full cursor-pointer">
+                                            <input {...getInputProps()} />
+                                            <div className="flex flex-col items-center justify-center">
+                                                <svg
+                                                    className="w-8 h-8 mb-4 text-gray-500 "
+                                                    aria-hidden="true"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="none"
+                                                    viewBox="0 0 20 16">
+                                                    <path
+                                                        stroke="currentColor"
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        strokeWidth="2"
+                                                        d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                                                    />
+                                                </svg>
+                                                <p className="mb-2 text-sm text-gray-500 ">
+                                                    <span className="font-semibold">Add Image</span>
+                                                </p>
+                                                <p className="text-xs text-gray-500 flex justify-center">
+                                                    PNG, JPG or GIF (1:1)
+                                                </p>
                                             </div>
-                                        )}
-                                    </Dropzone>
-                                    {/* <label
-                                        htmlFor="dropzone-file"
-                                        className="flex flex-col items-center justify-center w-full h-full cursor-pointer ">
-                                        <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                                            <svg
-                                                className="w-8 h-8 mb-4 text-gray-500 "
-                                                aria-hidden="true"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                fill="none"
-                                                viewBox="0 0 20 16">
-                                                <path
-                                                    stroke="currentColor"
-                                                    strokeLinecap="round"
-                                                    strokeLinejoin="round"
-                                                    strokeWidth="2"
-                                                    d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                                                />
-                                            </svg>
-                                            <p className="mb-2 text-sm text-gray-500 ">
-                                                <span className="font-semibold">Add Image</span>
-                                            </p>
-                                            <p className="text-xs text-gray-500 flex justify-center">
-                                                PNG, JPG or GIF (1:1)
-                                            </p>
                                         </div>
-                                        <input
-                                            multiple
-                                            accept="image/*"
-                                            id="dropzone-file"
-                                            type="file"
-                                            className="hidden"
-                                            onChange={uploadImage}
-                                        />
-                                    </label> */}
-                                </div>
+                                    )}
+                                </Dropzone>
                             </div>
                         </div>
                     </SortableContext>
